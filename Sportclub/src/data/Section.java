549a881e1b7941b69b7f197312cd5662edc42a84
Elena <eg.sabi@gmail.com>
2019-11-09 21:32:40 +0100

    added methods to base class

--- Sportclub/src/data/Section.java
+++ Sportclub/src/data/Section.java
@@ -1,4 +1,23 @@
 package data;
 
+
 public abstract class Section {
+    private String namesection;
+    private String trainer;
+    private int basicprice;
+
+    public Section (String namesection, String trainer, int basicprice){
+        this.namesection=namesection;
+        this.trainer=trainer;
+        this.basicprice=basicprice;
+    }
+
+    public String getNamesection() {
+        return namesection;
+    }
+
+    public String getTrainer() {
+        return trainer;
+    }
 }
+
