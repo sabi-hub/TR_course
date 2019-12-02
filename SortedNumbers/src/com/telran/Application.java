eb2d3e5669eac0180360529b2d6680652d98780c
Elena <eg.sabi@gmail.com>
2019-12-02 21:18:52 +0100

    ready SNL

--- SortedNumbers/src/com/telran/Application.java
+++ SortedNumbers/src/com/telran/Application.java
@@ -9,7 +9,6 @@ public class Application {
     public static void main(String[] args) {
 
         int[] javaArray = new int[]{1,2,-8,3};
-        int[]anotherArray = new int[]{1, 4, 3};
 
 //        SortedNumbersList sortedArray = new SortedNumbersList(javaArray);
 //        System.out.println(Arrays.toString(new SortedNumbersList[]{sortedArray}));
