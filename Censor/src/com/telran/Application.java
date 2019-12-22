1fce44ea0d2a9c8ea07412091ac8d3ffdf9239f6
Elena <eg.sabi@gmail.com>
2019-12-22 12:41:17 +0100

    ready Censor

--- Censor/src/com/telran/Application.java
+++ Censor/src/com/telran/Application.java
@@ -15,5 +15,40 @@ public class Application {
 
         System.out.println(vasya.isLegal(text));
         System.out.println(vasya.isLegal(text2));
+
+//how works method clone()
+        Human[]humans1 = {
+                new Human("Vasya", 20),
+                new Human("Petya", 30)
+        };
+
+        Human []humans2 = humans1.clone();
+        humans1[0].name = "Vasiliy"; //changes name in originl and in clone array
+
+        System.out.println(humans1[0]);
+        System.out.println(humans2[0]);
+        System.out.println();
+
+        humans1[0] = new Human("Vasisualiy", 25); // changes only original array
+        System.out.println(humans1[0]);
+        System.out.println(humans2[0]);
+    }
+}
+
+class Human{
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
