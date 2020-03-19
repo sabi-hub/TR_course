4f8a6365c4b84caa693e7b69ec99a6a6a49f9cd3
Elena <eg.sabi@gmail.com>
2020-03-19 22:30:12 +0100

    now incoming and outcoming in executor

--- 20_03_19_udp_server/src/com/telran/ServerProcessor.java
+++ 20_03_19_udp_server/src/com/telran/ServerProcessor.java
@@ -7,35 +7,36 @@ import java.net.DatagramSocket;
 public class ServerProcessor implements Runnable {
     static final int INCOMING_DATAGRAM_SIZE = 1024;
     private DatagramSocket socket;
-    private DatagramPacket incomingPacket;
-    byte[] incomingData;
 
-    public ServerProcessor(DatagramSocket socket, DatagramPacket incomingPacket, byte[] incomingData) {
+    public ServerProcessor(DatagramSocket socket) {
         this.socket = socket;
-        this.incomingPacket = incomingPacket;
-        this.incomingData = incomingData;
     }
 
     @Override
     public void run() {
-        try {
-//            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
-//            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);//входящие пакеты
-//            socket.receive(incomingPacket);
+        while(true) {
+            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
+            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);
 
-            //откуда данные, с какого индекса (0), размер от пакета (сколько в итоге пришло байт)
+            try {
+                socket.receive(incomingPacket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
             String line = new String(incomingData, 0, incomingPacket.getLength());
             line = "Hello from UDP server " + line;
 
-            //отпркавка сообщения обратно
-            DatagramPacket outgoingPacket = new DatagramPacket(line.getBytes(), line.length()
-                    , incomingPacket.getAddress(), incomingPacket.getPort());
+            DatagramPacket outgoingPacket = new DatagramPacket(line.getBytes()
+                    , line.length()
+                    , incomingPacket.getAddress()
+                    , incomingPacket.getPort());
 
-            socket.send(outgoingPacket);
-//            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                socket.send(outgoingPacket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
     }
 }
