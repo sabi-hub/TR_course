5461e17d104760286e2545d11d1e8da13be1e311
Elena <eg.sabi@gmail.com>
2020-03-19 16:55:05 +0100

    ready

--- 20_03_15_cropped_text/src/com/telran/CroppedTextTest.java
+++ 20_03_15_cropped_text/src/com/telran/CroppedTextTest.java
@@ -12,7 +12,27 @@ public class CroppedTextTest {
         String text = "hello world hi to all";
         int k = 10;
         String result = ct.getCroppedText(text,k);
-        String expected = "hello ";
+        String expected = "hello";
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void testGetCroppedText_allWords_equalsK(){
+        String text = "hello world";
+        int k = 11;
+        String result = ct.getCroppedText(text,k);
+        String expected = "hello world";
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void testGetCroppedText_allWords_kMoreText(){
+        String text = "hello world";
+        int k = 21;
+        String result = ct.getCroppedText(text,k);
+        String expected = "hello world";
 
         assertEquals(expected,result);
     }
