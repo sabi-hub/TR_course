9886cdd72eceef93cdc933772c6108823557440c
Elena <eg.sabi@gmail.com>
2020-03-20 09:48:11 +0100

    Revert "now incoming and outcoming in executor"
    This reverts commit 4f8a6365c4b84caa693e7b69ec99a6a6a49f9cd3.

--- 20_03_19_udp_server/src/com/telran/Main.java
+++ 20_03_19_udp_server/src/com/telran/Main.java
@@ -1,10 +1,8 @@
 package com.telran;
 
-import javax.print.attribute.standard.RequestingUserName;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.ServerSocket;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,31 +13,31 @@ public class Main {
     static final int PORT = 3000;
 
     public static void main(String[] args) throws IOException {
+
         DatagramSocket socket = new DatagramSocket(PORT);
 
-        //Multithreading
         ExecutorService executor = Executors.newFixedThreadPool(10);
-        ServerProcessor sp = new ServerProcessor(socket);
-        executor.execute(sp);
 
+        while (true) {
 
-        //udp server without multithreading
-//        while (true) {
-//            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];//максимальный размер, какой может быть
-//            // куда записать и размер пакеты
-//            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);//входящие пакеты
-//            socket.receive(incomingPacket);
+            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];//максимальный размер, какой может быть
+            // куда записать и размер пакеты
+            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);//входящие пакеты
+            socket.receive(incomingPacket);
 //
-////            //откуда данные, с какого индекса (0), размер от пакета (сколько в итоге пришло байт)
-//            String line = new String(sp.incomingData, 0,sp.incomingPacket.getLength());
+//            //откуда данные, с какого индекса (0), размер от пакета (сколько в итоге пришло байт)
+//            String line = new String(incomingData, 0,incomingPacket.getLength());
 //            line = "Hello from UDP server " + line;
 //
 //            //отпркавка сообщения обратно
-//
 //            DatagramPacket outgoingPacket = new DatagramPacket(line.getBytes(), line.length()
-//                    ,sp.incomingPacket.getAddress(), sp.incomingPacket.getPort());
+//                    ,incomingPacket.getAddress(), incomingPacket.getPort());
 //            socket.send(outgoingPacket);
-//        }
+
+            ServerProcessor sp = new ServerProcessor(socket, incomingPacket, incomingData);
+            executor.execute(sp);
+
+        }
 
     }
 }
--- 20_03_19_udp_server/src/com/telran/ServerProcessor.java
+++ 20_03_19_udp_server/src/com/telran/ServerProcessor.java
@@ -7,36 +7,35 @@ import java.net.DatagramSocket;
 public class ServerProcessor implements Runnable {
     static final int INCOMING_DATAGRAM_SIZE = 1024;
     private DatagramSocket socket;
+    private DatagramPacket incomingPacket;
+    byte[] incomingData;
 
-    public ServerProcessor(DatagramSocket socket) {
+    public ServerProcessor(DatagramSocket socket, DatagramPacket incomingPacket, byte[] incomingData) {
         this.socket = socket;
+        this.incomingPacket = incomingPacket;
+        this.incomingData = incomingData;
     }
 
     @Override
     public void run() {
-        while(true) {
-            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
-            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);
+        try {
+//            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
+//            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);//входящие пакеты
+//            socket.receive(incomingPacket);
 
-            try {
-                socket.receive(incomingPacket);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            //откуда данные, с какого индекса (0), размер от пакета (сколько в итоге пришло байт)
             String line = new String(incomingData, 0, incomingPacket.getLength());
             line = "Hello from UDP server " + line;
 
-            DatagramPacket outgoingPacket = new DatagramPacket(line.getBytes()
-                    , line.length()
-                    , incomingPacket.getAddress()
-                    , incomingPacket.getPort());
+            //отпркавка сообщения обратно
+            DatagramPacket outgoingPacket = new DatagramPacket(line.getBytes(), line.length()
+                    , incomingPacket.getAddress(), incomingPacket.getPort());
 
-            try {
-                socket.send(outgoingPacket);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            socket.send(outgoingPacket);
+//            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
