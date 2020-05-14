7cd5a32621ef9c680cc396c4ca9b582e463f7732
esabitova <eg.sabi@gmail.com>
2020-05-14 22:29:59 +0200

    hw

--- heroes-tutorial/src/app/heroes/heroes.component.ts
+++ heroes-tutorial/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Hero} from "../hero";
-import {HEROES} from "../mock-heroes";
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,6 +9,7 @@ import {HEROES} from "../mock-heroes";
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero: Hero;
 
   hero: Hero = {
     id: 1,
@@ -21,4 +22,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
